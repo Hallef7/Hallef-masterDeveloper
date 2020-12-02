9e81af4e733fa5d06b84555f57b0d79292351025
Hallef7 <58997008+Hallef7@users.noreply.github.com>
2020-12-02 09:56:46 -0300

    Update Main.java

--- logica-metodo2/src/main/java/logica/metodo2/Main.java
+++ logica-metodo2/src/main/java/logica/metodo2/Main.java
@@ -1,26 +1,3 @@
-/*******************************************************************************
- * MIT License
- *
- * Copyright (c) 2019 Softblue
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *******************************************************************************/
 package logica.metodo2;
 
 import br.com.softblue.commons.io.Console;
@@ -32,7 +9,7 @@ public class Main {
 		System.out.print("Digite a temperatura: ");
 		double temp = Console.readDouble();
 
-		System.out.print("Qual a conversão? (1 = F -> C, 2 = C -> F): ");
+		System.out.print("Qual a conversÃ£o? (1 = F -> C, 2 = C -> F): ");
 		int opcao = Console.readInt();
 
 		double r;
@@ -44,11 +21,11 @@ public class Main {
 			r = converterParaFahrenheit(temp);
 
 		} else {
-			System.out.println("Opção inválida");
+			System.out.println("OpÃ§Ã£o invÃ¡lida");
 			return;
 		}
 
-		System.out.println("A temperatura convertida é: " + r);
+		System.out.println("A temperatura convertida Ã©: " + r);
 	}
 
 	static double converterParaCelsius(double fahrenheit) {
